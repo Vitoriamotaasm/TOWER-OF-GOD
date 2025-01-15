695213bd120a148b2f3aec178451b77e2c1afa64
Vitoriamotaasm <luanavitoriadasilvamota@gmail.com>
2025-01-14 22:45:48 -0300

    Criando modelo de dados

---
+++ src/app/models/item.model.ts
@@ -0,0 +1,9 @@
+// src/app/models/item.model.ts
+
+export interface Item {
+    id: string;           // Identificador único do item
+    name: string;         // Nome do item
+    description: string;  // Descrição do item
+    // Adicione outros campos conforme necessário
+  }
+  
