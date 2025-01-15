fa41c6af874f79c0b5c9d1d56f157d1a974b1905
Vitoriamotaasm <luanavitoriadasilvamota@gmail.com>
2025-01-14 22:41:14 -0300

    integrando componentes no layout principal

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,25 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HeaderComponent } from './shared/header/header.component';
+import { FooterComponent } from './shared/footer/footer.component';
 import { RouterOutlet } from '@angular/router';
+import { AuthService } from './auth/auth.service/auth.sevice';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
 
 @Component({
     selector: 'app-root',
-    imports: [RouterOutlet],
+    standalone: true,
+    imports: [CommonModule, HeaderComponent, FooterComponent,RouterOutlet],
     templateUrl: './app.component.html',
     styleUrl: './app.component.scss'
 })
+
 export class AppComponent {
-  title = 'tower-of-god';
+  isLoggedIn$: Observable<boolean>;
+
+  constructor(private authService: AuthService) {
+    this, this.isLoggedIn$ =
+    this.authService.getUser().pipe(map(user => !!user));
+  }
 }
