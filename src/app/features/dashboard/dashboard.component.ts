2b6da19e593dd62806ed7c576a708e348ff72f3d
Vitoriamotaasm <luanavitoriadasilvamota@gmail.com>
2025-01-14 22:43:15 -0300

    Criando componente de dashboard (tela inicial do jogo)

--- src/app/features/dashboard/dashboard.component.ts
+++ src/app/features/dashboard/dashboard.component.ts
@@ -1,11 +1,33 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { loadItems } from '../../core/store/actions/item.actions';
+import { Observable } from 'rxjs';
+import { selectAllItems, selectItemsError, selectItemsLoading } from '../../core/store/selectors/item.selectors';
+import { Item } from '../../models/item.model';
+import { ScoreService } from '../../core/services/score.service';
 
 @Component({
     selector: 'app-dashboard',
-    imports: [],
+    standalone: true,
+    imports: [CommonModule],
     templateUrl: './dashboard.component.html',
     styleUrl: './dashboard.component.scss'
 })
 export class DashboardComponent {
+items$: Observable<Item[]>;
+loading$: Observable<boolean>;
+error$: Observable<any>;
+score$: Observable<number> | undefined;
 
+constructor(private store: Store, private scoreService: ScoreService) {
+    this.items$ = this.store.select(selectAllItems);
+    this.loading$ = this.store.select(selectItemsLoading);
+    this.error$ = this.store.select(selectItemsError)
+    this.score$ = this.scoreService.score$;
+  }
+
+  ngOnInit(): void {
+    this.store.dispatch(loadItems());
+  }
 }
